a57820d7e1ec20a9f9f21fadf343c62ae7ab4529
Ythildir <pivardjulien@gmail.com>
2020-02-03 13:51:35 +0100

    Mise en place formule

--- src/executeur/executeur_g-executer.adb
+++ src/executeur/executeur_g-executer.adb
@@ -1,8 +1,27 @@
+with Ada.Text_IO;
+with Ada.Numerics.Generic_Elementary_Functions;
+
 separate (Executeur_G)
 procedure Executer
    --  (Arguments)
 is
+   type Calcul_T is digits 5;
+
+   package Math_IO is new Ada.Text_IO.Float_IO (Calcul_T);
+   package Math_P  is new Ada.Numerics.Generic_Elementary_Functions
+      (Calcul_T);
+
+   X : Calcul_T := 5.0;
+   D : Calcul_T := 2.0;
+   R : Calcul_T;
 begin
-   --  Mettez votre code ici
-   null;
+   R := 10.0 + (X**2) - 10.0 * Math_P.Cos (X => 2.0 * Ada.Numerics.Pi * X);
+
+   Math_IO.Put          (Item    => R, Fore => 3, Aft => 3, Exp => 0);
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   R := Ada.Numerics.Pi * ((D**2) / 2.0) + 4.0 * (160.0 / D);
+
+   Math_IO.Put          (Item    => R, Fore => 3, Aft => 3, Exp => 0);
+   Ada.Text_IO.New_Line (Spacing => 1);
 end Executer;
