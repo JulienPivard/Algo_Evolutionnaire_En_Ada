80f78ee69a27d67696fa341944da38ef8bbc27df
Ythildir <pivardjulien@gmail.com>
2020-02-13 23:18:25 +0100

    Suppression intervalle devenu inutile

--- src/a_e/intervalle/a_e_p-intervalle_p.ads
+++ src/a_e/intervalle/a_e_p-intervalle_p.ads
@@ -15,8 +15,4 @@ is
    type Indice_T is range 1 .. 25;
    --  L'intervalle de valeurs de la population.
 
-   subtype Intervalle_Initial_T is V_Param_T range 0.0 .. 1100.0;
-   --  Permet de définir l'intervalle de valeurs dans lequel
-   --  va évoluer la variable de la fonction.
-
 end A_E_P.Intervalle_P;
