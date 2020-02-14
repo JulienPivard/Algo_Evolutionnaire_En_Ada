1a6bd1abfae72111d459d2c8f884b10eed9ccf84
Ythildir <pivardjulien@gmail.com>
2020-02-14 02:28:04 +0100

    Ajout aléatoire dans accouplement valeur

--- src/a_e/valeur_param/a_e_p-valeur_param_p.adb
+++ src/a_e/valeur_param/a_e_p-valeur_param_p.adb
@@ -1,3 +1,5 @@
+with Ada.Numerics.Discrete_Random;
+
 with Generateur_G;
 
 pragma Elaborate_All (Generateur_G);
@@ -6,7 +8,13 @@ package body A_E_P.Valeur_Param_P
    with Spark_Mode => Off
 is
 
-   package Generateur_P is new Generateur_G (Valeur_T => V_Param_T);
+   type Repartition_Caractere_T is (Pere, Mere, Moyenne);
+
+   package Generateur_P       is new Generateur_G (Valeur_T => V_Param_T);
+   package Alea_Repartition_P is new Ada.Numerics.Discrete_Random
+      (Result_Subtype => Repartition_Caractere_T);
+
+   Generateur_Repartition : Alea_Repartition_P.Generator;
 
    ---------------------------------------------------------------------------
    procedure Generer
@@ -29,9 +37,19 @@ is
       )
       return Valeur_Param_T
    is
+      Origine : constant Repartition_Caractere_T :=
+         Alea_Repartition_P.Random (Gen => Generateur_Repartition);
+
       Bebe : Valeur_Param_T;
    begin
-      Bebe.Valeur := (Parametre.Valeur + Autre.Valeur) / 2.0;
+      case Origine is
+         when Pere =>
+            Bebe.Valeur := Parametre.Valeur;
+         when Mere =>
+            Bebe.Valeur := Autre.Valeur;
+         when Moyenne =>
+            Bebe.Valeur := (Parametre.Valeur + Autre.Valeur) / 2.0;
+      end case;
       return Bebe;
    end Accoupler;
    ---------------------------------------------------------------------------
@@ -57,5 +75,9 @@ is
       return Parametre.Valeur;
    end Lire_Valeur;
    ---------------------------------------------------------------------------
+
+begin
+
+   Alea_Repartition_P.Reset (Gen => Generateur_Repartition);
 
 end A_E_P.Valeur_Param_P;
