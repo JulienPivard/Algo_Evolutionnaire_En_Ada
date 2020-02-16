8a752a3bbb0d7d61633a739a03098afca01a0bf9
Ythildir <pivardjulien@gmail.com>
2020-02-16 13:37:23 +0100

    Ajout procédure générique pour faciliter exemples

--- src/executeur/executeur_g-executer.adb
+++ src/executeur/executeur_g-executer.adb
@@ -17,6 +17,60 @@ separate (Executeur_G)
 procedure Executer
    --  (Arguments)
 is
+
+   ---------------------------------------------------------------------------
+   generic
+      with package Population_P is new A_E_P.Algo_Evolutionnaire_G (<>);
+   procedure Determiner_Min;
+
+   procedure Determiner_Min is
+      Population : Population_P.Population_T;
+      Debut, Fin : Ada.Real_Time.Time;
+
+      Nb_Generations : Natural := Natural'First;
+   begin
+      Population_P.Afficher_Details;
+
+      Population_P.Initialiser (Population => Population);
+
+      Ada.Text_IO.Put_Line (Item => "========== Valeurs de départ ==========");
+
+      Ada.Text_IO.New_Line    (Spacing => 1);
+      Population_P.Put_Line   (Item    => Population);
+      Ada.Text_IO.New_Line    (Spacing => 1);
+
+      Population_P.Faire_Evoluer
+         (
+            Population      => Population,
+            Debut           => Debut,
+            Fin             => Fin,
+            Nb_Generations  => Nb_Generations
+         );
+
+      Ada.Text_IO.Put_Line (Item => "======= Valeurs après évolution =======");
+      Ada.Text_IO.Put_Line (Item => "La valeur de X pour le min : 5.88");
+
+      Ada.Text_IO.New_Line    (Spacing => 1);
+      Population_P.Put_Line   (Item    => Population);
+      Ada.Text_IO.New_Line    (Spacing => 1);
+
+      Ada.Text_IO.Put_Line
+         (Item => "Nombre de générations : " & Natural'Image (Nb_Generations));
+
+      --------------------------------------
+      Ada.Text_IO.New_Line (Spacing => 1);
+      --  Affiche le temps de filtrage du fichier.
+      Ada.Text_IO.Put      (Item    => "Temps total : ");
+      Ada.Text_IO.New_Line (Spacing => 1);
+
+      --  Conversion du temps pour faciliter l'affichage.
+      Chrono_P.Affichage_Temps (Debut => Debut, Fin => Fin);
+
+      Ada.Text_IO.New_Line (Spacing => 1);
+      --------------------------------------
+   end Determiner_Min;
+   ---------------------------------------------------------------------------
+
    package Population_Surface_P  is new A_E_P.Algo_Evolutionnaire_G
       (
          Indice_Population_T => Intervalle_P.Indice_T,
