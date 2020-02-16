b1ada9748d73e64ea1883477f9d82e93afeb3ba4
Ythildir <pivardjulien@gmail.com>
2020-02-16 02:48:16 +0100

    Ajustement nom variable

--- src/executeur/executeur_g-executer.adb
+++ src/executeur/executeur_g-executer.adb
@@ -21,39 +21,40 @@ is
          Put                 => A_E_P.Parametres_P.Surface_P.Text_IO.Put
       );
 
-   Population : Population_Surface_P.Population_T;
-   Debut, Fin : Ada.Real_Time.Time;
+   Population_Surface : Population_Surface_P.Population_T;
+   Debut, Fin         : Ada.Real_Time.Time;
 
    Nb_Generations : Natural := Natural'First;
 begin
    Population_Surface_P.Afficher_Details;
 
-   Population_Surface_P.Initialiser (Population => Population);
+   Population_Surface_P.Initialiser (Population => Population_Surface);
 
    Ada.Text_IO.Put_Line (Item    => "========== Valeurs de départ ==========");
 
    Ada.Text_IO.New_Line          (Spacing => 1);
-   Population_Surface_P.Put_Line (Item    => Population);
+   Population_Surface_P.Put_Line (Item    => Population_Surface);
    Ada.Text_IO.New_Line          (Spacing => 1);
 
    Debut := Ada.Real_Time.Clock;
    Boucle_Generation_Successive :
    loop
 
-      Population_Surface_P.Trier (Population => Population);
+      Population_Surface_P.Trier (Population => Population_Surface);
 
       --  Toutes les valeurs survivantes doivent se trouver autour
       --  de la valeur minimum du tableau +/-1
       --  Intervalle de convergence
       exit Boucle_Generation_Successive when
-         Population_Surface_P.Verifier_Convergence (Population => Population);
+         Population_Surface_P.Verifier_Convergence
+            (Population => Population_Surface);
 
       Nb_Generations := Nb_Generations + 1;
 
-      Population_Surface_P.Remplacer_Morts (Population => Population);
+      Population_Surface_P.Remplacer_Morts (Population => Population_Surface);
 
       Population_Surface_P.Calcul_Formule_Sur_Enfant
-         (Population => Population);
+         (Population => Population_Surface);
 
    end loop Boucle_Generation_Successive;
    Fin := Ada.Real_Time.Clock;
@@ -61,7 +62,7 @@ begin
    Ada.Text_IO.Put_Line (Item    => "======= Valeurs après évolution =======");
 
    Ada.Text_IO.New_Line          (Spacing => 1);
-   Population_Surface_P.Put_Line (Item    => Population);
+   Population_Surface_P.Put_Line (Item    => Population_Surface);
    Ada.Text_IO.New_Line          (Spacing => 1);
 
    Ada.Text_IO.Put_Line
