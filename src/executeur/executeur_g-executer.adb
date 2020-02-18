fcb8f01005058d500e49a942bd709147f7bec0e9
Ythildir <pivardjulien@gmail.com>
2020-02-19 00:32:17 +0100

    Indentation

--- src/executeur/executeur_g-executer.adb
+++ src/executeur/executeur_g-executer.adb
@@ -128,9 +128,12 @@ is
    procedure Min_Anonyme_2 is new Determiner_Min
       (Population_P => Population_Anonyme_2_P);
 begin
-   Min_Surface    (Nom => "X", Min => "5.88");
+   Min_Surface
+      (Nom => "X",      Min => "5.88");
 
-   Min_Anonyme_1  (Nom => "X", Min => "0.0");
+   Min_Anonyme_1
+      (Nom => "X",      Min => "0.0");
 
-   Min_Anonyme_2  (Nom => "X et Y", Min => "-0.55, -1,55");
+   Min_Anonyme_2
+      (Nom => "X et Y", Min => "-0.55, -1,55");
 end Executer;
