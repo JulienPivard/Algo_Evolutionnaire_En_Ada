6ae060e4ab63520bf2927f445fbb20aac483d090
Ythildir <pivardjulien@gmail.com>
2020-02-18 19:15:12 +0100

    Ajout de la date et heure de compilation

--- src/client.adb
+++ src/client.adb
@@ -1,11 +1,13 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Dimanche 02 février[02] 2020
+--           Dernière modification : Mardi 18 février[02] 2020
 --                                                                          --
 ------------------------------------------------------------------------------
 
 with Ada.Text_IO;
+with GNAT.Source_Info;
+
 with Executeur_G;
 
 procedure Client is
@@ -17,6 +19,11 @@ procedure Client is
       );
 
 begin
+
+   Ada.Text_IO.Put      (Item => "Date de compilation : ");
+   Ada.Text_IO.Put      (Item => GNAT.Source_Info.Compilation_ISO_Date);
+   Ada.Text_IO.Put      (Item => " ");
+   Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Compilation_Time);
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
