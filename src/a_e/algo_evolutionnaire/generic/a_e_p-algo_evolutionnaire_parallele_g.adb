107344d54485ac332b7ff175003239a696022e11
Ythildir <pivardjulien@gmail.com>
2021-06-24 08:12:53 +0200

    Faire évoluer fait démarrer les ilots

--- src/a_e/algo_evolutionnaire/generic/a_e_p-algo_evolutionnaire_parallele_g.adb
+++ src/a_e/algo_evolutionnaire/generic/a_e_p-algo_evolutionnaire_parallele_g.adb
@@ -12,31 +12,15 @@ is
          Nb_Generations :    out Natural
       )
    is
-      Nb_Tours_Sans_Divergences : Outils_P.Nb_Tours_Sans_Divergences_T := 0;
    begin
-      Outils_P.Initialiser (Population => Population.Pop);
-
       Nb_Generations := Natural'First;
       Debut          := Ada.Real_Time.Clock;
 
       Outils_P.Initialiser (Population => Population.Pop);
 
-      Boucle_Generation_Successive :
-      loop
-         Outils_P.Trier_Et_Verifier
-            (
-               Population             => Population.Pop,
-               Tours_Sans_Divergences => Nb_Tours_Sans_Divergences
-            );
-
-         exit Boucle_Generation_Successive when Nb_Tours_Sans_Divergences = 25;
-         exit Boucle_Generation_Successive when Nb_Generations = Natural'Last;
-
-         Nb_Generations := Nb_Generations + 1;
-
-         Outils_P.Passer_A_La_Generation_Suivante
-            (Population => Population.Pop);
-      end loop Boucle_Generation_Successive;
+      for E of Table_De_Demarreurs loop
+         E.Demarrer (Population => Population);
+      end loop;
 
       Fin := Ada.Real_Time.Clock;
    end Faire_Evoluer;
