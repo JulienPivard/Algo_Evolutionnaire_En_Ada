19e150eff316174df21d0af86f6fd9c884d2ae30
Ythildir <pivardjulien@gmail.com>
2021-10-12 09:07:06 +0200

    génération chemins tient compte restriction d'unicité

--- src/demo/chemin_le_plus_court_dans_graphe/chemin/evo/demo_p-chemin_p-evo_p.adb
+++ src/demo/chemin_le_plus_court_dans_graphe/chemin/evo/demo_p-chemin_p-evo_p.adb
@@ -20,10 +20,32 @@ is
    procedure Generer
       (Parametres : in out Probleme_Chemin_T)
    is
+      type Apparition_Sommets_T is array (Sommets_T) of Boolean;
+
+      Sommets_Deja_Utilise : Apparition_Sommets_T :=
+         Apparition_Sommets_T'(others => False);
+
+      Sortir_Boucle_Trouver_Sommet : Boolean := False;
    begin
       Boucle_Construire_Chemin :
       for S of Parametres.Chemin.Sommets loop
          S := Alea_Sommets_P.Random (Gen => Generateur_Sommets);
+
+         Boucle_Trouver_Sommet_Libre :
+         loop
+            Sortir_Boucle_Trouver_Sommet := not Sommets_Deja_Utilise (S);
+            if Sommets_Deja_Utilise (S) then
+               if S = Sommets_T'Last then
+                  S := Sommets_T'First;
+               else
+                  S := Sommets_T'Succ (S);
+               end if;
+            else
+               Sommets_Deja_Utilise (S) := True;
+            end if;
+
+            exit Boucle_Trouver_Sommet_Libre when Sortir_Boucle_Trouver_Sommet;
+         end loop Boucle_Trouver_Sommet_Libre;
       end loop Boucle_Construire_Chemin;
    end Generer;
    ---------------------------------------------------------------------------
