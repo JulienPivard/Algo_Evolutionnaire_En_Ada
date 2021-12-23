c8fd37c41a2578d006a51c2c53f10035eca86773
Ythildir <pivardjulien@gmail.com>
2021-12-23 17:40:27 +0100

    Utilisation de sommet_io

--- src/demo/chemin_le_plus_court_dans_graphe/graphe/io/demo_p-graphe_p-text_io.adb
+++ src/demo/chemin_le_plus_court_dans_graphe/graphe/io/demo_p-graphe_p-text_io.adb
@@ -18,13 +18,15 @@ is
 
       Boucle_Afficher_Ligne :
       for S_L in Sommets_T loop
-         Ada.Text_IO.Put (Item => S_L'Img & " ");
+         Sommet_IO.Put   (Item => S_L);
+         Ada.Text_IO.Put (Item => " ");
       end loop Boucle_Afficher_Ligne;
       Ada.Text_IO.New_Line (Spacing => 1);
 
       Boucle_Parcourir_Ligne :
       for S_L in Sommets_T loop
-         Ada.Text_IO.Put (Item => S_L'Img & " ");
+         Sommet_IO.Put   (Item => S_L);
+         Ada.Text_IO.Put (Item => " ");
 
          Boucle_Parcourir_Colonne :
          for S_C in Sommets_T loop
@@ -40,7 +42,8 @@ is
                Ada.Text_IO.Put (Item => "- ");
             end if;
          end loop Boucle_Parcourir_Colonne;
-         Ada.Text_IO.Put (Item => " " & S_L'Img);
+         Ada.Text_IO.Put (Item => " ");
+         Sommet_IO.Put   (Item => S_L);
          Ada.Text_IO.New_Line (Spacing => 1);
       end loop Boucle_Parcourir_Ligne;
 
@@ -48,7 +51,8 @@ is
 
       Boucle_Afficher_Ligne_Fin :
       for S_L in Sommets_T loop
-         Ada.Text_IO.Put (Item => S_L'Img & " ");
+         Sommet_IO.Put   (Item => S_L);
+         Ada.Text_IO.Put (Item => " ");
       end loop Boucle_Afficher_Ligne_Fin;
       Ada.Text_IO.New_Line (Spacing => 1);
    end Put_Line;
