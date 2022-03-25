32dd7e319a67c7d3bab11e2ec88ca373fb0e03fb
Ythildir <pivardjulien@gmail.com>
2022-03-25 15:22:07 +0100

    Espaceement

--- src/client.adb
+++ src/client.adb
@@ -14,7 +14,7 @@ with Executeur_G;
 procedure Client is
 
    package Version_Compilateur is new GNAT.Compiler_Version;
-   package Executeur_P is new Executeur_G
+   package Executeur_P         is new Executeur_G
       (
          Nombre_D_Arguments_Min => 0,
          Nombre_D_Arguments_Max => 0
